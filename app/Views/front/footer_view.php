58de3a11529a09c5f449582f756320268027ebb9
Rodrigo Argota <rodrigoargota@gmail.com>
2025-06-24 23:45:02 -0300

    first commit: subiendo proyecto

---
+++ app/Views/front/footer_view.php
@@ -0,0 +1,56 @@
+    <!-- footer -->
+    <footer class="pie-pagina">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 mb-4">
+                    <h5>TechNova</h5>
+                    <p>Soluciones tecnológicas innovadoras para el futuro digital. Transformamos ideas en realidad digital.</p>
+                    <div class="social-links">
+                        <a href="#" class="text-white me-3"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="text-white me-3"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="text-white me-3"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 mb-4">
+                    <h6>Servicios</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white-50">Desarrollo Web</a></li>
+                        <li><a href="#" class="text-white-50">Inteligencia Artificial</a></li>
+                        <li><a href="#" class="text-white-50">Consultoría</a></li>
+                        <li><a href="#" class="text-white-50">Soporte</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-6 mb-4">
+                    <h6>Empresa</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white-50">Acerca de</a></li>
+                        <li><a href="#" class="text-white-50">Equipo</a></li>
+                        <li><a href="#" class="text-white-50">Carreras</a></li>
+                        <li><a href="#" class="text-white-50">Contacto</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <h6>Suscríbete a nuestro newsletter</h6>
+                    <p class="text-white-50">Recibe las últimas noticias sobre tecnología e innovación.</p>
+                    <form class="d-flex">
+                        <input type="email" class="form-control me-2" placeholder="Tu email">
+                        <button class="btn btn-outline-light" type="submit">Suscribir</button>
+                    </form>
+                </div>
+            </div>
+            <hr class="my-4">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <p class="mb-0">&copy; 2025 TechNova. Todos los derechos reservados.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+
+</body>
+</html>
---
+++ app/Views/front/navbar_view.php
@@ -0,0 +1,49 @@
+<!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="principal.html">
+                <img src="img/logo.png" alt="Logo" width="40" height="40" class="me-2">
+                TechNova
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="principal.html">Principal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#quienes">Quiénes Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#acerca">Acerca de</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            Servicios
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Desarrollo Web</a></li>
+                            <li><a class="dropdown-item" href="#">Inteligencia Artificial</a></li>
+                            <li><a class="dropdown-item" href="#">Consultoría</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Todos los Servicios</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#registro">Registrarse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#login">Login</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Buscar servicios..." aria-label="Search">
+                    <button class="btn btn-outline-light" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>
